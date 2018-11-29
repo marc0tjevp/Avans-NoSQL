0bd01b41ecc03fc5006f1f4e4f7cc4c249cddf7a
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-11-29 23:21:23 +0100

    Code cleanup and variable checks

--- controller/thread.controller.js
+++ controller/thread.controller.js
@@ -5,171 +5,232 @@ const auth = require('../config/authentication.config')
 const neodb = require('../neodb/neodbhelper')
 
 function getThreadById(req, res) {
-	
-	//Get params
+
+	// Get params
 	let threadId = req.params.id || ''
 
-	Thread.findOne({ _id: threadId },{ comments: 0 },
-		function(err, thread){
-			if(thread){
-			res.status(200).json(new ApiResponse(200,thread)).end()
-			} else{
-				res.status(404).json(new ApiResponse(404,"Thread not found")).end()
+	// Check if ID exists
+	if (threadId == '') {
+		res.status(400).json(new ApiResponse(400, "Provide the parameters: id"))
+	}
+
+	Thread.findOne({
+			_id: threadId
+		}, {
+			comments: 0
+		},
+		function (err, thread) {
+			if (thread) {
+				res.status(200).json(new ApiResponse(200, thread)).end()
+			} else {
+				res.status(404).json(new ApiResponse(404, "Thread not found")).end()
 			}
 		}
 	)
-	
+
 }
 
 function getKarma(req, res) {
 
-	//Get params
+	// Get params
 	let id = req.params.id || ''
 
-	if(id == ''){
-		res.status(400).json(new ApiResponse(400, "No threadId given"))
+	// Check if ID exists
+	if (id == '') {
+		res.status(400).json(new ApiResponse(400, "Provide the parameters: id"))
 	}
-	neodb.getThreadDownvotes(res, id ,(downVotes)=>{
-		neodb.getThreadUpvotes(res, id ,(upVotes)=>{
-			res.status(200).json(new ApiResponse(200,"Karma = " + upVotes + "  - " + downVotes + " = " + (upVotes - downVotes))).end()
+
+	// Get Karma
+	neodb.getThreadDownvotes(res, id, (downVotes) => {
+		neodb.getThreadUpvotes(res, id, (upVotes) => {
+			res.status(200).json(new ApiResponse(200, "Karma = " + upVotes + "  - " + downVotes + " = " + (upVotes - downVotes))).end()
 		})
 	})
 }
 
 function voteUp(req, res) {
 
-	//Get params
+	// Get params
 	let id = req.params.id || ''
 
-	//Get token
+	// Get token
 	var token = req.get('Authorization') || ''
-	var decodedUsername = auth.decodeToken(token) || ''
-
-	if(id == ''){
-		res.status(400).json(new ApiResponse(400, "No threadId given"))
+	var decodedUsername
+	if (token != '') {
+		decodedUsername = auth.decodeToken(token)
 	}
 
-	neodb.createThreadUpvote(res, decodedUsername.sub , id ,()=>{
-		res.status(200).json(new ApiResponse(200, decodedUsername.sub+" upvoted the thread with id: " + id)).end()
+	// Check if ID exists
+	if (id == '') {
+		res.status(400).json(new ApiResponse(400, "Provide the parameters: id"))
+	}
+
+	// Create upvote
+	neodb.createThreadUpvote(res, decodedUsername.sub, id, () => {
+		res.status(200).json(new ApiResponse(200, decodedUsername.sub + " upvoted the thread with id: " + id)).end()
 	})
 }
 
 function voteDown(req, res) {
 
-	//Get params
+	// Get params
 	let id = req.params.id || ''
 
-	//Get token
+	// Get token
 	var token = req.get('Authorization') || ''
-	var decodedUsername = auth.decodeToken(token) || ''
-
-	if(id == ''){
-		res.status(400).json(new ApiResponse(400, "No threadId given"))
+	var decodedUsername
+	if (token != '') {
+		decodedUsername = auth.decodeToken(token)
 	}
 
-	neodb.createThreadDownvote(res,decodedUsername.sub , id ,()=>{
-		res.status(200).json(new ApiResponse(200, decodedUsername.sub+" downvoted the thread with id: " + id)).end()
+	// Check if ID exists
+	if (id == '') {
+		res.status(400).json(new ApiResponse(400, "Provide the parameters: id"))
+	}
+
+	// Create downvote
+	neodb.createThreadDownvote(res, decodedUsername.sub, id, () => {
+		res.status(200).json(new ApiResponse(200, decodedUsername.sub + " downvoted the thread with id: " + id)).end()
 	})
 }
 
 function create(req, res) {
-	
-	//Get params
+
+	// Get params
 	let t = req.body.title || ''
 	let con = req.body.content || ''
 
-	//Get token
+	// Check if ID exists
+	if (t == '' || con == '') {
+		res.status(400).json(new ApiResponse(400, "Provide the parameters: title, content"))
+	}
+
+	// Get token
 	var token = req.get('Authorization') || ''
-    var decodedUsername
-    if (token != '') {
-        decodedUsername= auth.decodeToken(token)
-    }
-		console.log(token)
-		User.findOne({ username: decodedUsername.sub },{_id:1},
-			function(err, u){
-				var thread = new Thread({
-					user: u._id,
-					title: t,
-					content: con
-				})
-				thread.save(function(err){
-					console.log(err)
-					Thread.findOne(thread,{},(err,t)=>{
-						neodb.saveThread(res,t._id,(err,thread)=>{
-						res.status(200).json(new ApiResponse(200,t)).end()
+	var decodedUsername
+	if (token != '') {
+		decodedUsername = auth.decodeToken(token)
+	}
+
+	// Create Thread
+	User.findOne({
+			username: decodedUsername.sub
+		}, {
+			_id: 1
+		},
+		function (err, u) {
+			var thread = new Thread({
+				user: u._id,
+				title: t,
+				content: con
+			})
+			thread.save(function (err) {
+				console.log(err)
+				Thread.findOne(thread, {}, (err, t) => {
+					neodb.saveThread(res, t._id, (err, thread) => {
+						res.status(200).json(new ApiResponse(200, t)).end()
 					})
-					})
-					
-					
 				})
-			}
-		)
-	
-	
+
+
+			})
+		}
+	)
+
+
 }
 
 function deleteById(req, res) {
-	
-	//Get params
+
+	// Get params
 	let id = req.params.id || ''
 
-	//Get token
+	// Check if ID exists
+	if (id == '') {
+		res.status(400).json(new ApiResponse(400, "Provide the parameters: id"))
+	}
+
+	// Get token
 	var token = req.get('Authorization') || ''
 	var decodedUsername
-	
-    if (token != '') {
-        decodedUsername= auth.decodeToken(token)
+	if (token != '') {
+		decodedUsername = auth.decodeToken(token)
 	}
-	
-	User.findOne({username: decodedUsername.sub},{},(err,u)=>{
-	
-		Thread.findOne({_id:id,user:u._id},{},(err,t)=>{
-			
-			if(t){
-				Thread.deleteOne(t,(err)=>{
-					res.status(200).json(new ApiResponse(200,"deleted")).end()
+
+	// Delete Thread
+	User.findOne({
+		username: decodedUsername.sub
+	}, {}, (err, u) => {
+
+		Thread.findOne({
+			_id: id,
+			user: u._id
+		}, {}, (err, t) => {
+
+			if (t) {
+				Thread.deleteOne(t, (err) => {
+					res.status(200).json(new ApiResponse(200, "deleted")).end()
 				})
-			} else{
-			res.status(401).json(new ApiResponse(401,"Not Authorised to delete"))
+			} else {
+				res.status(401).json(new ApiResponse(401, "Not Authorised to delete"))
 			}
 		})
 	})
 }
 
 function updateById(req, res) {
-	var token = req.get('Authorization') || ''
-	//Get params
+
+	// Get params
 	let id = req.params.id || ''
 	let newContent = req.body.content || ''
 
-	//Get token
-	
-	console.log("UPDATE "+token + " + id = "+id)
+	// Check if ID exists
+	if (id == '' || content == '') {
+		res.status(400).json(new ApiResponse(400, "Provide the parameters: id, content"))
+	}
+
+	// Get token
+	var token = req.get('Authorization') || ''
 	var decodedUsername
-	if(token!=''){
+	if (token != '') {
 		decodedUsername = auth.decodeToken(token)
 	}
-	User.findOne({username:decodedUsername.sub},{},(err,u)=>{
-		Thread.findOne({_id:id,user:u._id},{},(err,t)=>{
-			if(t){
-				
-				Thread.updateOne(t,{content:newContent},(err)=>{
-					res.status(200).json(new ApiResponse(200,"deleted")).end()
+
+	// Update Thread
+	User.findOne({
+		username: decodedUsername.sub
+	}, {}, (err, u) => {
+		Thread.findOne({
+			_id: id,
+			user: u._id
+		}, {}, (err, t) => {
+			if (t) {
+
+				Thread.updateOne(t, {
+					content: newContent
+				}, (err) => {
+					res.status(200).json(new ApiResponse(200, "deleted")).end()
 				})
-			}else{
-			res.status(401).json(new ApiResponse(401,"Not Authorised to delete"))
+			} else {
+				res.status(401).json(new ApiResponse(401, "Not Authorised to delete"))
 			}
 		})
 	})
 }
 
 function getAll(req, res) {
-	//get params
+
+	// get params
 	let limit = req.query.limit || 25
 	let title = req.query.title || ''
-	 
-	Thread.find({title:new RegExp(title,"i")},{_id:0}).limit(limit).then((err,threads)=>{
-		res.status(200).json(new ApiResponse(200,threads)).end()
+
+	// Get all Threads, standard limit 25
+	Thread.find({
+		title: new RegExp(title, "i")
+	}, {
+		_id: 0
+	}).limit(limit).then((err, threads) => {
+		res.status(200).json(new ApiResponse(200, threads)).end()
 	})
 }
 
